b4596c57ce3e6ba5fdb23c95287f72769f606bb0
NebrasMatalkeh <nebrasmatalqah@gmail.com>
2026-01-13 21:10:32 +0300

    Update All

--- Presentation Layer/Program.cs
+++ Presentation Layer/Program.cs
@@ -18,9 +18,9 @@ namespace Presentation_Layer
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            //builder.Services.AddDbContext<AppDbContext>(options =>
-    //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-            builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+          
             builder.Services.AddScoped<IAuthService, AuthService>();
             builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<IStudentService, StudentService>();
